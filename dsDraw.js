6bf044fd0427448bf6742c13e6933ea3055162dd
Francisco Licea <francisco.licea@gmail.com>
2010-11-13 15:05:34 -0600

    refactor draw array, now calls drawElems and drawLinesBetweenElemns

--- dsDraw.js
+++ dsDraw.js
@@ -18,28 +18,11 @@ function drawArray(array, ctx){
   var arrayWidth = maxCellWidth(array) * array.length  
   
   ctx.strokeRect(START_X, START_Y, arrayWidth, ARRAY_HEIGHT);
-  drawCells(array, ctx)
-}
-
-function drawCells(array, ctx){
-  var cellWidth = maxCellWidth(array)
-  drawElemns(array, ctx)
+  drawElems(array, ctx)
   drawLinesBetweenElems(array, ctx) 
 }
 
-function drawLinesBetweenElems(array ,ctx){
- spaceBetween = maxCellWidth(array)
- numLines = array.length - 1
- for(i = 1 ; i <= numLines ; i++){
-  currX = START_X + spaceBetween * i 
-  ctx.beginPath()
-  ctx.moveTo(currX, START_Y)
-  ctx.lineTo(currX, START_Y + ARRAY_HEIGHT)
-  ctx.stroke()
- }
-}
-
-function drawElemns(array, ctx){
+function drawElems(array, ctx){
   cellWidth = maxCellWidth(array)
   currX = START_X
   currY = START_Y
@@ -56,6 +39,18 @@ function drawElemns(array, ctx){
     currX += cellWidth / 2
     currY += (ARRAY_HEIGHT / 2) + FONT / 2
   }
+}
+
+function drawLinesBetweenElems(array ,ctx){
+ spaceBetween = maxCellWidth(array)
+ numLines = array.length - 1
+ for(i = 1 ; i <= numLines ; i++){
+  currX = START_X + spaceBetween * i 
+  ctx.beginPath()
+  ctx.moveTo(currX, START_Y)
+  ctx.lineTo(currX, START_Y + ARRAY_HEIGHT)
+  ctx.stroke()
+ }
 }
 
 function maxCellWidth(array){
