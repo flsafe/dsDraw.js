07d1353f600350adf23b33a8bc81e8cdd920fb69
Francisco Licea <francisco.licea@gmail.com>
2010-11-13 14:51:25 -0600

    Refactor out a drawElemns function

--- dsDraw.js
+++ dsDraw.js
@@ -23,24 +23,41 @@ function drawArray(array, ctx){
 
 function drawCells(array, ctx){
   var cellWidth = maxCellWidth(array)
-  drawLines(array.length - 1, START_X, START_Y, cellWidth, ARRAY_HEIGHT, ctx) 
-  for( i = 1 ; i <= array.length ; i++){
-    currX = START_X + cellWidth * i
-    currY = START_Y
-    ctx.font = "20px monospace"
-    ctx.fillText( array[i-1], currX, currY, cellWidth )
-  }
+  drawElemns(array, ctx)
+  drawLinesBetweenElems(array, ctx) 
 }
 
-function drawLines(number, startX, startY, space, height, ctx){
- for(i = 1 ; i <= number ; i++){
-  currX = startX + space * i 
-  currY = startY 
+function drawLinesBetweenElems(array ,ctx){
+ spaceBetween = maxCellWidth(array)
+ numLines = array.length - 1
+ for(i = 1 ; i <= numLines ; i++){
+  currX = START_X + spaceBetween * i 
   ctx.beginPath()
-  ctx.moveTo(currX, currY)
-  ctx.lineTo(currX, currY + height)
+  ctx.moveTo(currX, START_Y)
+  ctx.lineTo(currX, START_Y + ARRAY_HEIGHT)
   ctx.stroke()
  }
+}
+
+var FONT = 20
+
+function drawElemns(array, ctx){
+  cellWidth = maxCellWidth(array)
+  currX = START_X
+  currY = START_Y
+  currX += cellWidth / 2
+  currY += (ARRAY_HEIGHT / 2) + FONT / 2
+
+  for( i = 1 ; i <= array.length ; i++){
+    ctx.font = FONT + "px monospace"
+    ctx.textAlign = "center"
+    ctx.fillText( array[i-1], currX, currY, cellWidth )
+    currX = START_X + cellWidth * i
+    currY = START_Y
+
+    currX += cellWidth / 2
+    currY += (ARRAY_HEIGHT / 2) + FONT / 2
+  }
 }
 
 var PIXELS_PER_DIGIT = 25
